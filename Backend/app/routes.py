8066713051be1eb13dc9f4ae602db80ead3e95a6
Akshat Singh <akshatsingh511@gmail.com>
2025-08-03 20:06:35 +0530

    Build functionality of text to voice conversion

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,6 +1,15 @@
+import os
+from dotenv import load_dotenv
 from fastapi import APIRouter, Request
+from murf import Murf
 
+load_dotenv()
 router = APIRouter()
+
+API_KEY = os.getenv("API_KEY")
+STYLE = os.getenv("STYLE")
+VOICE_ID = os.getenv("VOICE_ID")
+
 
 @router.get("/")
 def index():
@@ -10,3 +19,33 @@ def index():
         "day": 1
     }
     return data
+
+@router.post("/getTTS")
+async def TTS(request: Request):
+
+    data = await request.json()
+    text = data.get("text" , "")
+
+    if not text:
+        return {
+            "status" : 400,
+            "error": "please enter some text to convert it into speech"
+        }
+
+    client = Murf(api_key=API_KEY)
+
+    response = client.text_to_speech.generate(
+        text = text,
+        voice_id = VOICE_ID,
+        style =  STYLE,
+        rate = 0
+    )
+
+    return {"audio_url" : response.audio_file}
+
+
+
+
+
+
+
--- Frontend/script.js
+++ Frontend/script.js
@@ -11,16 +11,36 @@ async function showAlert(){
 }
 
 
-async function gettts(){
+async function textToSpeech(){
+
+    const userText =  document.getElementById("user_input").value;
+
+    document.getElementById("user_input").value= "";
 
     try {
-        const response = await fetch("http://localhost:8000/tts");
+        const response = await fetch("http://localhost:8000/getTTS" , {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ text: userText }),
+        }
+            
+        );
 
         const data = await response.json();
 
-        console.log(data);
-    } catch (error) {
+        if(data){
+            let audio_is = document.getElementById("audio")
+
+            audio_is.src = data.audio_url;
+
+            audio_is.style.display = "block"
+        }
+        else{
+            alert("failed to convert text to voice");
+        }
         
+    } catch (error) {
+        console.log("Can't make request to TTS")
     }
 }
 
